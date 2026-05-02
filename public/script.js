28e9a5ebb78672149d4cd9c351665c90172c399a
Vasudxtt <vasuduttpareek@gmail.com>
2026-05-02 17:08:53 +0530

    spacew

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-<script>
+
     /* ──── PDF.js ──── */
     if (typeof pdfjsLib !== 'undefined') pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
     async function extractPDF(file) {
